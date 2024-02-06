145999aba5652d4fe78e1cd10323c2da4a20abcc
PenjagaBEBEK <158865147+PenjagaBEBEK@users.noreply.github.com>
2024-02-06 18:03:42 +0800

    db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
 	host: 'sql.freedb.tech',
 	dialect: 'mysql',
-    username: 'freedb_ihsun',
-    password: 'W!rJv*76khzExux',
-    database: 'freedb_CecepDB'
+    username: 'freedb_ihsan',
+    password: '?s&QuZ7dnfP8kNM',
+    database: 'freedb_todolist-ihsan'
 });
 
 module.exports = sequelize
